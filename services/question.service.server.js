31fa4bb37d8da86bdb7644584f6bf068382e6a49
Steve <zjysteve@gmail.com>
2020-04-14 00:19:35 -0600

    Update question.service.server.js

--- services/question.service.server.js
+++ services/question.service.server.js
@@ -1,11 +1,9 @@
-let questions = require('./questions.json')
-
-const findQuestionsForQuiz = (quizId) =>
-    questions.filter(question => question.quizId === quizId);
-
-const findAllQuestions = () => questions;
-
+const questionsDao = require('../daos/questions.dao.server')
+const findAllQuestions = () => questionsDao.findAllQuestions()
+const findQuestionById = (qid) => questionsDao.findQuestionById(qid)
+const findQuestionsForQuiz = (qid) => questionsDao.findQuestionsForQuiz(qid)
 module.exports = {
-    findQuestionsForQuiz,
-    findAllQuestions
-};
+    findAllQuestions,
+    findQuestionById,
+    findQuestionsForQuiz
+}
