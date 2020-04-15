c35d2caecd367fd99de61d9395a7090d459befa8
Steve <zjysteve@gmail.com>
2020-04-14 22:41:17 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+var bodyParser = require('body-parser')
 var express = require('express');
 var app = express();
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/whiteboard-jzheng', {useNewUrlParser: true, useUnifiedTopology: true});
 
-
-app.get('/', function (req, res) {
-    res.send("This is the Course Manager Server")
-});
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -16,9 +17,21 @@ app.use(function (req, res, next) {
     next();
 });
 
+var session = require('express-session')
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'any string'
+}))
+
 require('./controllers/quiz.controller.server')(app);
 require('./controllers/question.controller.server')(app);
+require('./controllers/quiz-attempts.controller.server')(app);
+require('./controllers/users.cotroller.server')(app);
+
+app.get('/', function (req, res) {
+    res.send("This is the Course Manager Server")
+});
 
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => console.log(`Listending on port ${port}...`));
