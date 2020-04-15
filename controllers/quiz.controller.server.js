a36561c05e399b1916ee637dec044e42855dde90
Steve <zjysteve@gmail.com>
2020-04-14 20:13:53 -0600

    Update quiz controller to use asynchronous functions

--- controllers/quiz.controller.server.js
+++ controllers/quiz.controller.server.js
@@ -1,14 +1,10 @@
 const quizService = require('../services/quiz.service.server');
 
 module.exports = function (app) {
-
-    app.get('/api/quizzes', (req, res) => {
-        res.send(quizService.findAllQuizzes())
-    });
-
-    app.get('/api/quizzes/:qid', (req, res) => {
-        const quizId = req.params.qid;
-        const quiz = quizService.findQuizById(quizId);
-        res.json(quiz);
-    });
+    app.get('/api/quizzes', (req, res) =>
+        quizService.findAllQuizzes()
+            .then(allQuizzes => res.json(allQuizzes)))
+    app.get('/api/quizzes/:qzid', (req, res) =>
+        quizService.findQuizById(req.params['qzid'])
+            .then(quiz => res.json(quiz)))
 }
