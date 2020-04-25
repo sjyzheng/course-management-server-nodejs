120d0b24f6221bcde50d8066baa755c455651e45
Steve <zjysteve@gmail.com>
2020-04-25 16:21:47 -0600

    Minor changes to CORS

--- server.js
+++ server.js
@@ -10,13 +10,29 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://wbdv-sp20-jz-server-node-a9.herokuapp.com/');
     res.header('Access-Control-Allow-Headers',
         'Content-Type, X-Requested-With, Origin');
     res.header('Access-Control-Allow-Methods',
         'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     next();
 });
+
+app.use(function (req,res, next) {
+    var allowedOrigins = ['http://localhost:4200', 'http://localhost:8020', 'https://wbdv-sp20-jz-client-angular.herokuapp.com/', `https://wbdv-sp20-jz-client-angular-a9.herokuapp.com/`];
+    var origin = req.headers.origin;
+    if(allowedOrigins.indexOf(origin) > -1){
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials',
+        'true');
+    next();
+});
+
 
 var session = require('express-session')
 app.use(session({
